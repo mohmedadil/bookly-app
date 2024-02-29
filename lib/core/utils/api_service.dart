36a13704de838acfb87d4be53785ed55f57b3582
mohmedadil <mohmedadil797@gmail.com>
2024-02-29 18:52:36 +0200

    add api services

--- lib/core/utils/api_service.dart
+++ lib/core/utils/api_service.dart
@@ -8,7 +8,6 @@ class ApiService {
 
   Future<Map<String, dynamic>> get({required String endpoint}) async {
     var response = await dio.get("$_baseurl$endpoint");
-
     return response.data;
   }
 }
--- lib/features/home/data/repos/home_repo_implementation.dart
+++ lib/features/home/data/repos/home_repo_implementation.dart
@@ -1,13 +1,28 @@
 import 'package:bookly/core/errors/failures.dart';
+import 'package:bookly/core/utils/api_service.dart';
 import 'package:bookly/features/home/data/models/book_model/book_model.dart';
 import 'package:bookly/features/home/data/repos/home_repo.dart';
 import 'package:dartz/dartz.dart';
 
 class HomeRepoImepl implements HomeRepo {
+  final ApiService apiService;
+
+  HomeRepoImepl(this.apiService);
   @override
-  Future<Either<Failure, List<BookModel>>> fetchBestSellerBooks() {
-    // TODO: implement fetchBestSellerBooks
-    throw UnimplementedError();
+  Future<Either<Failure, List<BookModel>>> fetchNewestBooks() async {
+    try {
+      var data = await apiService.get(
+          endpoint:
+              'volumes?Filtering=free-ebooks&Sorting=newest&q=subject:programming');
+      List<BookModel> books = [];
+      
+      for (var item in data['items']) {
+        books.add(BookModel.fromJson(item));
+      }
+      return Right(books);
+    } catch (e) {
+      return Left(ServerFailure());
+    }
   }
 
   @override
@@ -15,5 +30,4 @@ class HomeRepoImepl implements HomeRepo {
     // TODO: implement fetchfeaturedBooks
     throw UnimplementedError();
   }
-
 }
