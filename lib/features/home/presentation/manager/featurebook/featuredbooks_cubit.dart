3d2c6cb6e2145176e2c6558efd9ef2c3ab99b2e1
mohmedadil <mohmedadil797@gmail.com>
2024-03-02 17:35:53 +0200

    newest books cubit dome

--- lib/features/home/presentation/manager/featurebook/featuredbooks_cubit.dart
+++ lib/features/home/presentation/manager/featurebook/featuredbooks_cubit.dart
@@ -1,9 +1,21 @@
 import 'package:bloc/bloc.dart';
+import 'package:bookly/core/errors/failures.dart';
 import 'package:bookly/features/home/data/models/book_model/book_model.dart';
+import 'package:bookly/features/home/data/repos/home_repo.dart';
 import 'package:equatable/equatable.dart';
 
 part 'featuredbooks_state.dart';
 
 class FeaturedbooksCubit extends Cubit<FeaturedbooksState> {
-  FeaturedbooksCubit() : super(FeaturedbooksInitial());
+  FeaturedbooksCubit(this.homeRepo) : super(FeaturedbooksInitial());
+  final HomeRepo homeRepo;
+  Future<void> fetchFeaturedBooks() async {
+    emit(FeaturedbooksLoading());
+    var result = await homeRepo.fetchfeaturedBooks();
+    result.fold((failure) {
+      emit(FeaturedbooksFailure(failure.errMessage));
+    }, (books) {
+      emit(FeaturedbooksSuccess(books));
+    });
+  }
 }
